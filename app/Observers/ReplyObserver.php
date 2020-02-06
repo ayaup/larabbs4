7d65bfe3a3998ab72c78a6eff9a7850184217431
shkaodao <shkaodao@icloud.com>
2020-02-06 12:53:02 +0800

    消息通知列表

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,7 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
-
+use App\Notifications\TopicReplied;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -13,11 +13,13 @@ class ReplyObserver
     {
         $reply->content = clean($reply->content, 'user_topic_body');
     }
-    
+
     public function created(Reply $reply)
     {
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 
     public function updating(Reply $reply)
